4b7b92eb73bf1b8d4aa4fa4823f91984924f45e5
Adequm <adequm@bk.ru>
2022-02-08 23:53:05 +0300

    first

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+process.env.LOCATION = 'http://localhost:3000'
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: ['@babel/polyfill', './index.js'],
+  mode: process.env.NODE_ENV,
+  target: isProd ? 'browserslist' : 'web',
+  output: {
+    filename: '[id].[contenthash].js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  resolve: {
+    alias: {
+      core: path.resolve(__dirname, 'core'),
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  devServer: {
+    port: 3000,
+    hot: !isProd
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[id].[contenthash].css',
+    }),
+    ...['index', 'about'].map(page => {
+      return new HTMLWebpackPlugin({
+        filename: `${ page }.html`,
+        template: `!!ejs-compiled-loader!src/views/${ page }.ejs`,
+      });
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      { 
+        test: /\.(png|jpeg|jpg|gif|svg)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }]
+      },
+      {
+        test: /\.ejs$/,
+        // use: {
+        //   loader: "ejs-webpack-loader",
+        //   options: {
+        //     data: { title: 'http://localhost:3000/' },
+        //     htmlmin: true
+        //   }
+        // }
+        use: {
+          loader: 'ejs-compiled-loader',
+          options: {
+            data: { title: 'http://localhost:3000/' },
+            htmlmin: !isProd,
+            htmlminOptions: {
+              removeComments: !isProd
+            }
+          }
+        },
+      }
+    ]
+  }
+};
